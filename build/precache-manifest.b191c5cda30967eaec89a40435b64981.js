2380caf2ac5d24853213f251fe9e5b93f6eb18b4
Neha V <nehav9311@gmail.com>
2020-12-18 16:01:29 +0530

    First commit

---
+++ build/precache-manifest.b191c5cda30967eaec89a40435b64981.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72580650cc563ffadc3a1fb4d34be999",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cb11cc1bb94920a15a2",
+    "url": "/static/js/2.076fe5c5.chunk.js"
+  },
+  {
+    "revision": "9b1a7147eb78d4062b39",
+    "url": "/static/js/main.2f437c71.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
